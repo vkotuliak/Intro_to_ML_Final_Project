25582fb364ab116525fbcf0eea63e4823293a373
viktorkotuliak <viktor.kotuliak@gmail.com>
2025-01-25 16:11:01 +0100

    Added word2vec to naive bayes and added logistic regression model

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -12,7 +12,6 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from gensim.models import Word2Vec
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
 
 # import data
@@ -38,52 +37,44 @@ y_train = Encoder.fit_transform(y_train)
 y_val = Encoder.fit_transform(y_val)
 
 # # Vectorize the word data
-# Vectorizer = TfidfVectorizer()
+Vectorizer = TfidfVectorizer()
 
-# x_train_tfidf = Vectorizer.fit_transform(x_train.values.astype("U"))
-# x_test_tfidf = Vectorizer.transform(x_test.values.astype("U"))
-# x_val_tfidf = Vectorizer.transform(x_val.values.astype("U"))
-
-# Train Word2Vec model
-x_train = x_train.astype(str)
-x_test = x_test.astype(str)
-x_val = x_val.astype(str)
+x_train_tfidf = Vectorizer.fit_transform(x_train.values.astype("U"))
+x_test_tfidf = Vectorizer.transform(x_test.values.astype("U"))
+x_val_tfidf = Vectorizer.transform(x_val.values.astype("U"))
 
 # Train Word2Vec model
-sentences = [tweet.split() for tweet in x_train.tolist()]
-word2vec_model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
+# x_train = x_train.astype(str)
+# x_test = x_test.astype(str)
+# x_val = x_val.astype(str)
 
-# Function to transform tweets to Word2Vec vectors
-def tweet_to_vec(tweet, model):
-    words = tweet.split()
-    word_vecs = [model.wv[word] for word in words if word in model.wv]
-    return np.mean(word_vecs, axis=0) if word_vecs else np.zeros(model.vector_size)
+# # Train Word2Vec model
+# sentences = [tweet.split() for tweet in x_train.tolist()]
+# word2vec_model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
 
-# Vectorize the word data using Word2Vec
-x_train_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_train])
-x_test_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_test])
-x_val_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_val])
+# # Function to transform tweets to Word2Vec vectors
+# def tweet_to_vec(tweet, model):
+#     words = tweet.split()
+#     word_vecs = [model.wv[word] for word in words if word in model.wv]
+#     return np.mean(word_vecs, axis=0) if word_vecs else np.zeros(model.vector_size)
 
-scaler = MinMaxScaler()
-x_train_w2v = scaler.fit_transform(x_train_w2v)
-x_test_w2v = scaler.transform(x_test_w2v)
-x_val_w2v = scaler.transform(x_val_w2v)
+# # Vectorize the word data using Word2Vec
+# x_train_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_train])
+# x_test_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_test])
+# x_val_w2v = np.array([tweet_to_vec(tweet, word2vec_model) for tweet in x_val])
 
 # The Naive Bayes model
-# model = LogisticRegression(max_iter=1000)
-model = GaussianNB()
+model = LogisticRegression(max_iter=1000)
 
-# param_grid = {"C": [0.1, 1.0, 10.0], "penalty": ["l2"], "solver": ["lbfgs"]}
-param_grid = {'var_smoothing': [1e-9, 1e-8, 1e-7], 'priors': [None, [0.5, 0.5]]}
+param_grid = {"C": [0.1, 1.0, 10.0], "penalty": ["l2"], "solver": ["lbfgs"]}
 
 # Set up the grid search to find the best hyperparameters
 grid_search = GridSearchCV(model, param_grid, cv=5, scoring="accuracy")
-# grid_search.fit(x_train_tfidf, y_train)
-grid_search.fit(x_train_w2v, y_train)
+grid_search.fit(x_train_tfidf, y_train)
+# grid_search.fit(x_train_w2v, y_train)
 best_model = grid_search.best_estimator_
 
-# predictions = best_model.predict(x_val_tfidf)
-predictions = best_model.predict(x_val_w2v)
+predictions = best_model.predict(x_val_tfidf)
 
 print(f"Best parameters found: {grid_search.best_params_}")
 
